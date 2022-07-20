b08bfbfa795ab4bcd3831d24167643b6e97fd411
zprogrammer-code <skatboyza124@hotmail.com>
2022-07-20 09:45:20 -0600

    I still have to house the image tags successfully in my assets folder

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import logo from "/home/zprogrammercode/jb_moving/src/Assets/images/Jacobs Brothers Moving asnd storage.png";
-
+import Logo from "/home/zprogrammercode/jb_moving/src/Assets/images/Jacobs Brothers Moving asnd storage.png";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} alt="" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <img
+        alt="Logo"
+        src="/home/zprogrammercode/jb_moving/src/Assets/images/Jacobs Brothers Moving asnd storage.png"
+      >
+        {Logo}
+      </img>
     </div>
   );
 }
