158223d7591b0554dc66e35b2c5acac1b8c2b498
zprogrammer-code <skatboyza124@hotmail.com>
2022-07-23 17:48:30 -0600

    i am making sure all of my connections are good in order to connect my style pages properly, so far i have styled  and posted a nav bar and a header

--- src/App.js
+++ src/App.js
@@ -1,15 +1,58 @@
-import Logo from "/home/zprogrammercode/jb_moving/src/Assets/images/Jacobs Brothers Moving asnd storage.png";
-function App() {
+// import Logo from "/home/zprogrammercode/jb_moving/src/Assets/images/Jacobs Brothers Moving asnd storage.png";
+export default function App() {
   return (
-    <div>
+    <>
+      <html>
+        <head>
+          <link rel="stylesheet" type="text/css" href="style.css"></link>
+          <link rel="icon" type="image/png" href="SENCOR_Logo.ico"></link>
+          <title>SENCOR</title>
+        </head>
+        <body>
+          <div class="bg-div">
+            <img
+              class="logo-img"
+              src="SENCOR_Logo.jpg"
+              width="130"
+              height="130"
+              ALT="align box"
+              ALIGN="CENTER"
+            ></img>
+            <nav>
+              <ul>
+                <li>
+                  <a href="#">Monitoring</a>
+                </li>
+                <li>
+                  <a href="#">Process</a>
+                </li>
+                <li>
+                  <a href="#">Post and Create Email/Excel</a>
+                </li>
+                <li>
+                  <a href="#">Reports</a>
+                </li>
+                <li>
+                  <a href="#">Tools</a>
+                </li>
+                <li>
+                  <a href="#">Sales</a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </body>
+      </html>
+    </>
+  );
+}
+{
+  /* < <div>
       <img
         alt="Logo"
         src="/home/zprogrammercode/jb_moving/src/Assets/images/Jacobs Brothers Moving asnd storage.png"
       >
         {Logo}
       </img>
-    </div>
-  );
+    </div> */
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import ReactDOM from "react-dom/client";
+import reportWebVitals from "./reportWebVitals";
+import React from "react";
+import "./app.css";
+import App from "./App";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
