81270d176534a74cb80285bbf622c03c74bf3e14
zprogrammer-code <zprogrammercode@gmail.com>
2022-09-03 16:36:27 -0600

    I have starte to use jb_moving as a react project that i can practice my new micro-components in

--- src/Pages/Main/main.js
+++ src/Pages/Main/main.js
@@ -2,5 +2,11 @@ import React from "react";
 import Header from "/home/zprogrammercode/jb_moving/src/Components/header.js";
 
 export default function Main() {
-  return Header;
-}
+    return(
+        {
+            <div>
+                <Header />
+            </div>
+        };
+    )
+}                          
